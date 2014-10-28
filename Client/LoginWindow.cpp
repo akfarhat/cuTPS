37f99355e48b3c9f698a6eb3f35cdaeb45aa3623
Ahmad <amasoudd@gmail.com>
2014-10-28 17:56:04 -0400

    Remove unused student include

--- Client/LoginWindow.cpp
+++ Client/LoginWindow.cpp
@@ -1,8 +1,6 @@
 #include "loginwindow.h"
 #include "ui_loginwindow.h"
 
-#include "Student.h"
-
 LoginWindow::LoginWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::LoginWindow) {
 
     ui->setupUi(this);
