f8b7728e2d90183836363d4b2b7c16b7dac20fe0
zachuserid <z.renaud5@gmail.com>
2014-11-02 19:43:40 -0500

    changed login to test window on launch

--- cuTPS-client/main.cpp
+++ cuTPS-client/main.cpp
@@ -1,12 +1,12 @@
-#include "LoginWindow.h"
+#include "Tests.h"
 #include <QApplication>
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    LoginWindow w;
-    w.show();
+    Tests testWindow;
+    testWindow.show();
 
     return a.exec();
 }
