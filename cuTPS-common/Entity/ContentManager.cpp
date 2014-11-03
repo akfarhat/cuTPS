3451088edce25ed7416ff05937ca292ba3e98952
Ahmad <amasoudd@gmail.com>
2014-11-03 16:48:28 -0500

    Add getDetails() function for User abstract class and implement in Student and ContentManager classes

--- cuTPS-common/Entity/ContentManager.cpp
+++ cuTPS-common/Entity/ContentManager.cpp
@@ -1,6 +1,6 @@
 #include "ContentManager.h"
 
-ContentManager::ContentManager(QString name, QString username) : User(name, username) {
+ContentManager::ContentManager(int id, QString name, QString username) : User(id, name, username) {
 
 }
 
@@ -14,6 +14,17 @@ void ContentManager::setEmailAddress(QString newEmail) {
     emailAddress = newEmail;
 }
 
-QString ContentManager::getType() {
-    return "Content Manager";
+QString ContentManager::getDetails() {
+    QString details = "";
+    details += "Type: Content Manager\nUser ID: ";
+    details += QString::number(this->getUserId());
+    details += "\nUsername: ";
+    details += this->getUsername();
+    details += "\nName: ";
+    details += this->getName();
+    details += "\nEmail Address: ";
+    details += this->getEmailAddress();
+    details += "\n";
+
+    return details;
 }
